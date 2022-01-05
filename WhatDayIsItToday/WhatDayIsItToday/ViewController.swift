7f9f887d7e867ab3e9f15acac95ff7a5dcf21d78
SaniyeToy <75203610+SaniyeToy@users.noreply.github.com>
2022-01-05 10:53:59 +0300

    IOS-6 Create view and bind to viewController

--- WhatDayIsItToday/WhatDayIsItToday/ViewController.swift
+++ WhatDayIsItToday/WhatDayIsItToday/ViewController.swift
@@ -8,12 +8,24 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    
+    @IBOutlet weak var dateLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func dayButton(_ sender: UIButton) {
+    }
+    
+    @IBAction func monthButton(_ sender: UIButton) {
+    }
+    @IBAction func yearButton(_ sender: UIButton) {
+    }
+    
+    @IBAction func todayButton(_ sender: Any) {
+    }
 }
 
